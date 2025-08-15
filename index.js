044326d18c770a2d31f11948f84ad994b74cf4ff
BulidwithRiyaz <dev.riyaz.code@gmail.com>
2025-08-15 22:51:11 +0530

    normal commit

--- index.js
+++ index.js
@@ -18,18 +18,6 @@ app.get("/rolldice", (req, res) => {
     res.render("rollDice.ejs", {num , pathimg });
 });
 
-app.get("/ig/:username", (req , res) => {
-    let { username } = req.params;
-    const instaData =require("./data.json");
-    const data = instaData[username];
-    if(data){
-        res.render("insta.ejs", { data })
-    } else {
-        res.render("error.ejs");
-    }
-    
-});
-
 app.use("/",(req ,res) => {
     res.send("<h1>this is response fo page </h1>");
 });
