1fa15a85e3de5700282678985aab059ee93d4bc5
BulidwithRiyaz <dev.riyaz.code@gmail.com>
2025-08-15 21:01:39 +0530

    created backend -ejs demo files

---
+++ index.js
@@ -0,0 +1,37 @@
+const express =require("express");
+const app = express();
+app.set("view engine", "ejs");
+
+
+
+
+let port = 4000;
+
+app.listen(port , () => {
+    console.log(`listing the port ${port}`);
+});
+
+app.get("/rolldice", (req, res) => {
+    let num = Math.floor(Math.random() * 6) + 1;
+    res.render("rollDice.ejs", {num });
+});
+
+app.get("/ig/:username", (req , res) => {
+    let { username } = req.params;
+    const instaData =require("./data.json");
+    const data = instaData[username];
+    if(data){
+        res.render("insta.ejs", { data })
+    } else {
+        res.render("error.ejs");
+    }
+    
+});
+
+app.use("/",(req ,res) => {
+    res.send("<h1>this is response fo page </h1>");
+});
+
+
+
+
